4a4c311a0e3c67f2c0c8a52868a2757bb927c86b
jakubprzydatek <58308829+jakubprzydatek@users.noreply.github.com>
2022-04-06 21:00:24 +0200

    FLASHCARDS-001 init project

---
+++ src/main/java/com/flashcards/java_flashcards/JavaFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package com.flashcards.java_flashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashcards/java_flashcards/JavaFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards.java_flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
