9193aae9a4c58096c0f6ac4ec3f7c9fbc255715d
jakubprzydatek <58308829+jakubprzydatek@users.noreply.github.com>
2022-05-09 22:03:06 +0200

    FLASHCARDS swagger and swagger-ui added

--- src/main/java/com/flashcards/java_flashcards/controller/FlashcardRestController.java
+++ src/main/java/com/flashcards/java_flashcards/controller/FlashcardRestController.java
@@ -2,16 +2,25 @@ package com.flashcards.java_flashcards.controller;
 
 import com.flashcards.java_flashcards.model.entities.Flashcard;
 import com.flashcards.java_flashcards.services.FlashcardsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FlashcardRestController {
 
     private FlashcardsService flashcardsService;
 
-    @PostMapping("/api/flashcard")
+    @GetMapping("/flashcard")
+    public List<Flashcard> getFlashcards()
+    {
+        throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @PostMapping("/flashcard")
     public Flashcard addFlashcard(@RequestBody Flashcard flashcard)
     {
         return flashcardsService.addFlashcard(flashcard);
