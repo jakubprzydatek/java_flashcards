f725014b350a09b1b8088a813b83aa0fa8a34241
jakubprzydatek <58308829+jakubprzydatek@users.noreply.github.com>
2022-05-09 23:28:10 +0200

    FLASHCARDS categories entity added

--- src/main/java/com/flashcards/java_flashcards/model/entities/Category.java
+++ src/main/java/com/flashcards/java_flashcards/model/entities/Category.java
@@ -4,25 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "Flashcard")
-public class Flashcard {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "FLASHCARD_NAME", nullable = false)
-    private String name;
+    private String categoryName;
 
-    private String content;
+    private String categoryShortName;
 
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-
-
+    @OneToMany(mappedBy = "category")
+    private Set<Flashcard> flashcard;
 }
